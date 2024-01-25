4403e06823f8d7341dab6907043375ded03e9681
BinodLamichhane <binodlamichhane9866@gmail.com>
2024-01-25 04:41:53 -0500

    ui-improve

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -283,7 +283,7 @@ function Header() {
        
           {signin && (
             <form
-              className="absolute top-[15vh] ml-3 sm:left-[35%] border mr-3  p-4 md:p-8 md:px-11 rounded-lg bg-slate-200"
+              className="absolute top-[15vh]  left-1/2 -translate-x-1/2 border   p-4 md:p-8 md:px-11 rounded-lg bg-slate-200"
               onSubmit={handlSubmiteSignUp}
             >
               <label
@@ -372,7 +372,7 @@ function Header() {
           {login && (
             <form
               onSubmit={handleSubmitLogin}
-              className="absolute top-[20vh] left-[10vw] md:left-[35vw] border p-4 md:p-8 md:px-11 rounded-lg bg-slate-200"
+              className="absolute top-[20vh]  left-1/2 -translate-x-1/2 border p-4 md:p-8 md:px-11 rounded-lg bg-slate-200"
             >
               <label
                 onClick={() => setLogin(!login)}
